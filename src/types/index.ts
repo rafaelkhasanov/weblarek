ee60f1a8423b9d53d30a0df4050f39a41b6a02a9
rnkhasanov <RNKhasanov@kzn.beeline.ru>
2026-04-26 00:26:55 +0300

    Создание необходимых типов

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,41 @@ export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
+
+export interface IProduct {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
+}
+
+export interface IBuyer {
+    payment: TPayment | null;
+    email: string;
+    phone: string;
+    address: string;
+}
+
+export type TPayment = "upon receipt" | "online" | '';
+
+export type ProductList = {
+    total: number;
+    items: IProduct[]
+}
+
+export type CreateOrder = {
+    payment: TPayment;
+    email: string;
+    phone: string;
+    address: string;
+    total: number;
+    items: string[]
+}
+
+export type CreateOrderSuccess = {
+    id: string;
+    total: number;
+}
+
