9c43e8f4df73c99a6f3595e76706bcb4dccbb9d1
rnkhasanov <RNKhasanov@kzn.beeline.ru>
2026-05-07 15:10:33 +0300

    Добавлена документация для класса CardBasket

--- src/components/views/CardBasket.ts
+++ src/components/views/CardBasket.ts
@@ -1,10 +1,22 @@
 import { ensureElement } from "../../utils/utils";
 import { Card, ICard, ICardActions } from "./Card";
 
+/**
+ * Класс карточки товара в корзине покупателя.
+ * Наследуется от абстрактного класса Card<ICardBasket> и предоставляет
+ * функциональность для отображения позиции товара в корзине и кнопки удаления.
+ */
 export class CardBasket extends Card<ICardBasket> {
+  /** Кнопка удаления товара из корзины */
   protected deleteButton: HTMLButtonElement;
+  /** Элемент отображения номера позиции товара в корзине */
   protected indexElement: HTMLElement;
 
+  /**
+   * Создаёт экземпляр карточки товара в корзине.
+   * @param cardBasket - корневой DOM-элемент LI контейнера карточки
+   * @param actions - обработчик клика по кнопке удаления
+   */
   constructor(cardBasket: HTMLLIElement, actions?: ICardActions) {
     super(cardBasket);
 
@@ -23,11 +35,21 @@ export class CardBasket extends Card<ICardBasket> {
     }
   }
 
+  /**
+   * Устанавливает отображение номера позиции товара в корзине.
+   * @param value - номер позиции товара (начиная с 1)
+   */
   set index(value: number) {
     this.indexElement.textContent = String(value);
   }
 }
 
+/**
+ * Интерфейс данных карточки товара в корзине.
+ * Расширяет интерфейс ICard и добавляет поле для номера позиции.
+ */
 export interface ICardBasket extends ICard {
+  /** Номер позиции товара в корзине */
   index: number;
 }
+
