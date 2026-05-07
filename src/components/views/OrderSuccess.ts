73ab4d9d504fcdb5220d92caf592da65d1dd8e90
rnkhasanov <RNKhasanov@kzn.beeline.ru>
2026-05-07 16:03:24 +0300

    Добавлена документация для класса OrderSuccess

--- src/components/views/OrderSuccess.ts
+++ src/components/views/OrderSuccess.ts
@@ -1,10 +1,22 @@
 import { ensureElement } from "../../utils/utils";
 import { Component } from "../base/Component";
 
+/**
+ * Класс сообщения об успешном оформлении заказа.
+ * Наследуется от Component<IOrderSuccess> и отображает информацию
+ * об успешно выполненном заказе и сумму списанных синапсов.
+ */
 export class OrderSuccess extends Component<IOrderSuccess> {
+  /** Элемент отображения описания успеха заказа */
   private orderSuccessDescription: HTMLElement;
+  /** Кнопка закрытия сообщения об успехе */
   private orderSuccessButton: HTMLButtonElement;
 
+  /**
+   * Создаёт экземпляр сообщения об успешном заказе.
+   * @param orderSuccess - корневой DOM-элемент контейнера сообщения
+   * @param actions - обработчик клика по кнопке закрытия
+   */
   constructor(orderSuccess: HTMLElement, actions?: IOrderSuccessActions) {
     super(orderSuccess);
     this.orderSuccessDescription = ensureElement<HTMLElement>(
@@ -25,15 +37,32 @@ export class OrderSuccess extends Component<IOrderSuccess> {
     }
   }
 
+  /**
+   * Устанавливает отображение информации об успешном заказе.
+   * Формирует сообщение с указанием суммы списанных синапсов.
+   * @param value - общая сумма заказа в синапсах
+   */
   set totalPrice(value: number) {
     this.orderSuccessDescription.textContent = `Списано ${value ?? 0} синапсов`;
   }
 }
 
+/**
+ * Интерфейс обработчиков событий сообщения об успешном заказе.
+ * Содержит callback-функции для обработки событий.
+ */
+export interface IOrderSuccessActions {
+  /** Обработчик клика по кнопке закрытия сообщения */
+  onOrderSuccessClick(): void;
+}
+
+/**
+ * Интерфейс данных сообщения об успешном заказе.
+ * Определяет структуру данных, которые могут быть переданы
+ * в метод render для отображения в компоненте OrderSuccess.
+ */
 export interface IOrderSuccess {
+  /** Общая сумма заказа в синапсах */
   totalPrice: number;
 }
 
-export interface IOrderSuccessActions {
-  onOrderSuccessClick(): void;
-}
