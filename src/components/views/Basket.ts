b4e33cf9b7773f9908208d4f632f7b7744655a17
rnkhasanov <RNKhasanov@kzn.beeline.ru>
2026-05-07 14:59:45 +0300

    Добавлена документация по классу представления Basket и Component

--- src/components/views/Basket.ts
+++ src/components/views/Basket.ts
@@ -1,11 +1,25 @@
 import { ensureElement } from "../../utils/utils";
 import { Component } from "../base/Component";
 
+/**
+ * Класс Basket отвечает за отображение корзины покупателя.
+ * Наследуется от базового класса Component<IBasket>.
+ * Управляет списком товаров в корзине, отображением общей стоимости
+ * и кнопкой оформления заказа.
+ */
 export class Basket extends Component<IBasket> {
+  /** Список товаров в корзине (элемент <ul>) */
   protected basketList: HTMLUListElement;
+  /** Кнопка оформления заказа */
   protected orderButton: HTMLButtonElement;
+  /** Элемент отображения общей стоимости заказа */
   protected priceElement: HTMLElement;
 
+  /**
+   * Создаёт экземпляр корзины.
+   * @param basket - корневой DOM-элемент контейнера корзины
+   * @param actions - объект с обработчиками событий корзины
+   */
   constructor(basket: HTMLElement, actions?: IBasketActions) {
     super(basket);
     this.basketList = ensureElement<HTMLUListElement>(
@@ -26,21 +40,43 @@ export class Basket extends Component<IBasket> {
     }
   }
 
+  /**
+   * Устанавливает список товаров в корзине.
+   * Если список пуст, кнопка оформления заказа блокируется.
+   * @param value - массив DOM-элементов карточек товаров
+   */
   set basketItems(value: HTMLElement[]) {
     this.orderButton.disabled = value.length < 1;
     this.basketList.replaceChildren(...value);
   }
 
+  /**
+   * Устанавливает отображение общей стоимости заказа.
+   * Если значение не определено, отображается 0.
+   * @param value - общая стоимость товаров в синапсах
+   */
   set price(value: number) {
     this.priceElement.textContent = `${value ?? 0} синапсов`;
   }
 }
 
+/**
+ * Интерфейс данных корзины.
+ * Определяет структуру данных, которые могут быть переданы
+ * в метод render для отображения в компоненте Basket.
+ */
 export interface IBasket {
+  /** Массив DOM-элементов товаров в корзине */
   basketItems: HTMLElement[];
+  /** Общая стоимость товаров в корзине */
   price: number;
 }
 
+/**
+ * Интерфейс обработчиков событий корзины.
+ * Содержит callback-функции для обработки пользовательских действий.
+ */
 export interface IBasketActions {
+  /** Обработчик нажатия на кнопку оформления заказа */
   onOrderClick(): void;
 }
