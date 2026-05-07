3bf69415c5924f314d422948a0520aa7e351d39c
rnkhasanov <RNKhasanov@kzn.beeline.ru>
2026-05-07 14:30:05 +0300

    Добавлена генерация событий в класс Catalog, авторефакторинг

--- src/components/models/Catalog.ts
+++ src/components/models/Catalog.ts
@@ -1,39 +1,49 @@
 ﻿import { IProduct } from "../../types";
+import { IEvents } from "../base/Events";
 
 export class Catalog {
-    private products: Map<string, IProduct>;
-    private openedProduct: IProduct | null;
+  private products: Map<string, IProduct>;
+  private openedProduct: IProduct | null;
+  private events: IEvents;
 
-    constructor() {
-        this.products = new Map<string, IProduct>();
-        this.openedProduct = null;
-    }
+  constructor(events: IEvents) {
+    this.products = new Map<string, IProduct>();
+    this.openedProduct = null;
+    this.events = events;
+  }
 
-    /** Заполнения каталога, принимает на вход массив IProduct */
-    setItems(items: IProduct[]): void {
-        this.products.clear();
-        items.forEach((product) => {
-            this.products.set(product.id, product);
-        })
-    }
+  /** Заполнения каталога, принимает на вход массив IProduct */
+  setItems(items: IProduct[]): void {
+    this.products.clear();
+    items.forEach((product) => {
+      this.products.set(product.id, product);
+    });
 
-    /** Получения массива товаров каталога */
-    getItems(): IProduct[] {
-        return [...this.products.values()];
-    }
+    this.events.emit("catalog:change");
+  }
 
-    /** Получение продукта из каталога по идентификатору */
-    getProductById(id: string): IProduct | undefined {
-        return this.products.get(id);
-    }
+  /** Получения массива товаров каталога */
+  getItems(): IProduct[] {
+    return [...this.products.values()];
+  }
 
-    /** Получение выбранного продукта */
-    getSelectedProduct(): IProduct | null {
-        return this.openedProduct;
-    }
+  /** Получение продукта из каталога по идентификатору */
+  getProductById(id: string): IProduct | undefined {
+    return this.products.get(id);
+  }
 
-    /** Установить выбранный продукт */
-    setSelectedProduct(id: string): void {
-        this.openedProduct = this.products.get(id) ?? null;
-    }
+  /** Получение выбранного продукта */
+  getSelectedProduct(): IProduct | null {
+    return this.openedProduct;
+  }
+
+  /** Установить выбранный продукт */
+  setSelectedProduct(id: string): void {
+    this.openedProduct = this.products.get(id) ?? null;
+    this.events.emit("catalog:change-selected-product");
+  }
+}
+
+export interface ICatalogActions {
+  onSetItems(): void;
 }
