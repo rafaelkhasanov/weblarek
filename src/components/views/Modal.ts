bf3d2c5eed82073e1a1cc3f3523106bb9edc6822
rnkhasanov <RNKhasanov@kzn.beeline.ru>
2026-05-07 15:51:56 +0300

    Добавлена документация для класса Modal

--- src/components/views/Modal.ts
+++ src/components/views/Modal.ts
@@ -1,10 +1,22 @@
 import { ensureElement } from "../../utils/utils";
 import { Component } from "../base/Component";
 
+/**
+ * Класс модального окна.
+ * Наследуется от Component<IModal> и предоставляет функциональность
+ * для отображения модального окна с кнопкой закрытия и контейнером контента.
+ */
 export class Modal extends Component<IModal> {
+  /** Кнопка закрытия модального окна */
   private modalCloseButton: HTMLButtonElement;
+  /** Контейнер для содержимого модального окна */
   private modalContent: HTMLElement;
 
+  /**
+   * Создаёт экземпляр модального окна.
+   * @param modal - корневой DOM-элемент контейнера модального окна
+   * @param actions - обработчик клика для закрытия модального окна
+   */
   constructor(modal: HTMLElement, actions?: IModalActions) {
     super(modal);
     this.modalCloseButton = ensureElement<HTMLButtonElement>(
@@ -26,15 +38,32 @@ export class Modal extends Component<IModal> {
     );
   }
 
+  /**
+   * Устанавливает содержимое модального окна.
+   * Заменяет все дочерние элементы контейнера модального окна переданным элементом.
+   * @param element - DOM-элемент для отображения в модальном окне
+   */
   set content(element: HTMLElement) {
     this.modalContent.replaceChildren(element);
   }
 }
 
+/**
+ * Интерфейс обработчиков событий модального окна.
+ * Содержит callback-функции для обработки событий модального окна.
+ */
+export interface IModalActions {
+  /** Обработчик клика для закрытия модального окна */
+  onClose(): void;
+}
+
+/**
+ * Интерфейс данных модального окна.
+ * Определяет структуру данных, которые могут быть переданы
+ * в метод render для отображения в компоненте Modal.
+ */
 export interface IModal {
+  /** Содержимое модального окна */
   content: HTMLElement;
 }
 
-export interface IModalActions {
-  onClose(): void;
-}
